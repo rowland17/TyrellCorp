0d7fcd4128a40c21707fc0badb4867a2beb7c9fd
Scott Rowland <rowland17@up.edu>
2015-11-24 11:09:57 -0800

    put files in public folder

---
+++ public/ui.js
@@ -0,0 +1,65 @@
+/**                                                                                                                                
+ * ui.js                                                                                                                           
+ *                                                                                                                                 
+ * Defines functionality for instrumenting the user-interface.                                                                     
+ *                                                                                                                                 
+ */
+var librs = librs || {};
+librs.ui = {};
+librs.ui = function() {
+var fetch = function() {
+	var el = document.getElementById('year');
+	var elVal = el.value;
+	console.log(elVal);
+	vizController(elVal);
+};
+var toggle = function() {
+
+    //console.log('About!');
+    
+    // Grab the html element with the ID “about”
+    var el = document.getElementById('ex0');
+	var el2 = document.getElementById('ex1');
+
+    // Add the class “show” to the element.
+    if(librs.utility.hasClass(el, 'show') && librs.utility.hasClass(el2, 'show'))
+    {
+   		librs.utility.removeClass(el, 'show');
+		librs.utility.removeClass(el2, 'show');
+    }
+    else
+    {
+    	librs.utility.addClass(el, 'show');
+		librs.utility.addClass(el2, 'show');
+    }
+    
+
+};
+
+var initialize = function() {
+
+    console.log('Initialize!');
+
+    // Grab the 'About' button element, identified by the                                                                          
+    // 'about-btn' id.                                                                                                             
+    var button = document.getElementById('graphBut');
+    //var btn = document.getElementById('submit-btn');
+
+    // From this point forward, when the button is clicked, the                                                                   
+    // toggle function shall be invoked.                                                                                           
+    button.onclick = toggle;
+   // btn.onclick = fetch;
+    
+
+};
+
+// When this file is included at the bottom of the page,                                                                           
+// the js is loaded after the DOM is loaded.  It is a                                                                              
+// good time to initialize the UI elements in the page.                                                                            
+initialize();
+
+}; //end module librs
+
+// Invoke module. After invocation, the module’s code is now added to
+// the namespace and is accessible through the librs object.                                                                                                                                                   
+librs.ui();
---
+++ public/viz.js
@@ -0,0 +1,113 @@
+/*
+ * viz.js
+ *
+ * Defines:
+ * - The data to be visualized in the chart.
+ * - The options for the look of the chart to be drawn.
+ * - How to draw the chart.
+ *
+ * @author: Tanya L. Crenshaw
+ * @author: Scott Rowland
+ * @since: Jan 6, 2015
+ */
+
+google.load('visualization', '1', {packages: ['corechart']});
+
+google.setOnLoadCallback(vizInit);
+
+var data;
+var chart;
+
+var views ;
+var totals = {};
+//var year = [2013, 2014];
+var options = {
+        width: 700,
+        height: 400,
+	title: 'Confidence and Satisfaction',
+        hAxis: {
+            title: 'Major',
+            gridlines: {count: 9}
+        },
+        vAxis: {
+            title: ''
+        },
+	legend: { 
+	    position: 'none' 
+	},
+	series: {
+		0: {color: '#551a8b'},
+		1: {color: '#999999'}
+	},
+	animation: {
+		"startup" : true,
+		"duration" : 500
+	}
+    };
+    
+    
+function vizInit() {
+
+	chart = new google.visualization.ColumnChart(document.getElementById('ex0'));
+    // STEP 3: STORE THE DATA.
+	var query= "SELECT MAJOR, AVE_CONF, SATISFACTION FROM 1n9vywcLACFd3BjZ8tqkwSPAMnxByOCpQwdCh_g0f";
+	
+	
+	var opts = {sendMethod: 'auto'};
+	var queryObj = new google.visualization.Query('https://www.google.com/fusiontables/gvizdata?tq=', opts);
+
+	// Send the query and handle the response by logging the data
+// to the console.                                                                
+	queryObj.setQuery(query);
+	queryObj.send(function(e) {
+
+            data = e.getDataTable();
+
+            console.log(data);
+
+            // Create a view for academic year 2013-2014 that                                                          
+            // is the first two columns of the data, just the                                                          
+            // rows that have 2013-2014 for the value.                                                                 
+
+            // First, get the textualized range of the year.                                                           
+            //var thisYear = "" + year[0] + "-" + year[1];
+
+            // Next, create the object and get the rows 
+// corresponding to "thisYear".                                   
+            views = new google.visualization.DataView(data);
+			
+
+            // Get a subset of the columns.                                                                            
+           // views.setColumns([0, 2]);
+
+			console.log(views.toDataTable());
+			
+            // Draw the chart for the initial academic year.                                                           
+            chart.draw(views.toDataTable(), options);
+			console.log(views);
+
+	});
+    
+}
+
+
+function vizController() {
+
+//    console.log(thisYear);
+	console.log(views);
+	if(views == undefined)
+	{
+		views = new google.visualization.DataView(data);
+        //views.setRows(views.getFilteredRows([{column: 2}]));
+//		views.setRows(views.getFilteredRows([{column: 1}]));		
+	//	views.setRows(views.getFilteredRows([{column: 2}]));
+
+            // Get a subset of the columns.                                                                            
+           // views.setColumns([0, 2]);
+
+            // Draw the chart for the initial academic year.                                                           
+            chart.draw(views.toDataTable(), options);
+	}
+console.log(views);
+
+}
